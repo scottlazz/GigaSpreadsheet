875bbfc674c0531f35f5c200c34a356bb6538342
scott <dikowitz@gmail.com>
2026-02-23 23:48:15 -0500

    app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // import Giga from "./giga";
 import Sheet from "./sheet";
 // import { Sheet } from './index';
-import { fininit } from "./_sampledata";
+import { fininit } from "./sampledata";
 import './style.css';
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const cells: any = [];
     const sheet = new Sheet(wrapper,
         Object.assign(
-            fininit,
+            // fininit,
             {
-                gridlinesOn: false,
+                // gridlinesOn: false,
                 // autosize: true,
                 // blockRows: 5,
                 // blockCols: 4,
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
         , {
             cellHeaders: true,
             autosize: true,
-            renderCustomCell: (cell: any, { left, top, width, height }: any) => {
-                console.log('rendering custom cell', cell, left, top, width, height);
-                const button = document.createElement('button');
-                button.textContent = 'Custom';
-                button.style.position = 'absolute';
-                sheet.setElRegistry(cell.row, cell.col, button, 'custom');
-                sheet.positionElement(button, left, top, width, height);
-            }
+            // renderCustomCell: (cell: any, { left, top, width, height }: any) => {
+            //     console.log('rendering custom cell', cell, left, top, width, height);
+            //     const button = document.createElement('button');
+            //     button.textContent = 'Custom';
+            //     button.style.position = 'absolute';
+            //     sheet.setElRegistry(cell.row, cell.col, button, 'custom');
+            //     sheet.positionElement(button, left, top, width, height);
+            // }
         })
     );
 });
